2bfc948cbe197bcfb9cd27c25d0f2d88a7256734
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-10 19:01:27 +0530

    update controller

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -1,1 +1,31 @@
 const userModel = require("../Models/user")
+const jwt = require("jsonwebtoken");
+
+const signup = async (req, res) => {
+    
+    const { username, email, password } = res.body;
+
+    try {
+        const isuserexist = await userModel.findOne({ email: email })
+        if (isuserexist) {
+            return res.status(400).json
+        }
+        const result = await userModel.create({
+            email: email,
+            password: password,
+            username:username
+        })
+
+        const token = jwt.sign({ email: result.email, id: result._id }, SECRET_KEY);
+        res.status(201).json({user : result,})
+    } catch (error) {
+        
+    }
+    
+};
+const signin = (req, res) => {
+  res.send("signin");
+};
+
+
+module.exports = {signin,signup};
--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,63 @@
-const express = require('express')
-const userRouter = express.Router();
+const express = require("express");
+const app = express();
+const http = require("http");
+const PORT = 3070;
+// console.log(process);
 
-userRouter.post("/signup", (req, res) => {
-    res.send("signup")
-})
-userRouter.post("/signin", (req, res) => {
-    res.send("SignIn")
-})
+app.use(express.json());
 
-module.exports = userRouter
+const todos = [];
+app.get("/", (req, res) => {
+  try {
+    return res.send({
+      todos,
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.send("internal error ");
+  }
+});
+
+app.post("/", (req, res) => {
+  try {
+    const { todo } = req.body;
+    const { task } = todo;
+    console.log(task);
+
+    let count = 0;
+    todos.forEach((todo) => {
+      count = Math.max(count, todo.id);
+    });
+
+    let data = {
+      id: count + 1,
+      task,
+      createdAt: new Date(),
+    };
+    todos.push(data);
+    return res.send("todo added");
+  } catch (err) {
+    console.error(err);
+    return res.status(500).send("internal server errr");
+  }
+});
+app.delete("/:id", (req, res) => {
+  const { id } = req.params;
+
+  let index = null;
+  todos.forEach((todo, i) => {
+    if (todo.id == id) {
+      index = i;
+    } else {
+      todos.splice(index, 1);
+      return res.send("deleted");
+    }
+  });
+  if (index == null) {
+    return res.status(404).send("data doesn't exist");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server started at http://localhost:${PORT}`);
+});
