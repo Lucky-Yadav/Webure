b02a2e2320de9d8311470bad6dd69c9fb6f5ca2a
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-11 00:58:17 +0530

    create redux

---
+++ src/redux/Register/action.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import {  REGISTER_REQ, SUCCESS_REQ } from "./actiontype";
+
+export const registerreq = (payload) => {
+  return {
+    type: REGISTER_REQ,
+    payload,
+  };
+};
+
+export const sucessreq = (payload) => {
+  return {
+    type: SUCCESS_REQ,
+    payload,
+  };
+};
+
+export const registeruser = (payload) => (dispatch) => {
+  console.log(payload, "after register");
+  dispatch(registerreq());
+  const requestParam = {
+    method: "post",
+    url: "https://reqres.in/api/login",
+    header: {
+      "Content-Type": "Application/json",
+    },
+    data: payload,
+  };
+  axios(requestParam)
+    .then(
+      (response) => console.log(response),
+      dispatch(sucessreq("REGISTER SUCESSSS"))
+    )
+    .catch((err) => console.log(err));
+};
---
+++ src/redux/Register/actiontype.js
@@ -0,0 +1,2 @@
+export const REGISTER_REQ = "REGISTER_REQ";
+export const SUCCESS_REQ = "SUCCESS_REQ";
