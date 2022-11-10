41e09130dca96769baf3173d92f55a26d8ec7ec3
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-10 11:58:26 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/home";
+import Login from "./components/login";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/Login" element={<Login />}></Route>
+      </Routes>
     </div>
   );
 }
