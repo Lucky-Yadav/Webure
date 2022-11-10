0d19d6900eb4e61c6cf1e2eda414caaa48b5ab96
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-10 12:26:48 +0530

    update context

--- src/context/AuthContext.jsx
+++
@@ -1,8 +0,0 @@
-import React, { createContext } from "react";
-
-export const AuthContext = createContext();
-
-export const AuthProvider = ({ children }) => {
-  // code here
-  return <AuthContext.Provider>{children}</AuthContext.Provider>;
-};
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-0;
+
 import { BrowserRouter } from "react-router-dom";
 
+import { AuthProvider } from "./context/StateManage";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
@@ -19,3 +20,6 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
