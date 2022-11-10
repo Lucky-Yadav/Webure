96818ea164f2da1fd0a0b4ae54587528537800a1
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-11-10 12:33:29 +0530

    Update Private.jsx

--- src/components/Private.jsx
+++ src/components/Private.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
 
 const Private = ({ children }) => {
-  const token = useSelector((state) => state.auth.token);
+  // const token = useSelector((state) => state.auth.token);
 
-  if (!token) {
+  // if (!token) {
     return <Navigate to={"/login"} />;
-  }
+  // }
   return children;
 };
 
